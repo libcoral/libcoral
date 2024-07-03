2aa2973605308b153d8802bd6c1515642f86df3f
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2024-07-03 10:59:22 +0200

    code cleanup

--- libcoral/src/coreset.rs
+++ libcoral/src/coreset.rs
@@ -1,7 +1,4 @@
 use ndarray::{concatenate, prelude::*, Data};
-use std::sync::Arc;
-
-trait FnWeight<A>: Fn(&Array1<usize>, &[A]) -> ArrayD<usize> + Send + Sync {}
 
 pub trait WeightCoresetPoints {
     fn weight_coreset_points<A>(
@@ -58,18 +55,6 @@ impl ExtractCoresetPoints for () {
         _assigned: &[usize],
     ) -> Array1<usize> {
         unreachable!("just to appease the type checker")
-    }
-}
-
-pub struct AncillaryInfo<'data, A> {
-    pub ancillary: &'data [A],
-    compute_weights_fn: Arc<dyn FnWeight<A>>,
-}
-
-impl<'data, A> AncillaryInfo<'data, A> {
-    fn compute_weights(&self, assignment: &Array1<usize>) -> ArrayD<usize> {
-        assert_eq!(assignment.len(), self.ancillary.len());
-        (self.compute_weights_fn)(assignment, self.ancillary)
     }
 }
 
@@ -294,96 +279,6 @@ impl<
     }
 }
 
-#[derive(Clone)]
-struct CoresetFit {
-    coreset_points: Array2<f32>,
-    radius: Array1<f32>,
-    weights: ArrayD<usize>,
-}
-
-impl Compose for CoresetFit {
-    fn compose(a: Self, b: Self) -> Self {
-        Self {
-            coreset_points: Compose::compose(a.coreset_points, b.coreset_points),
-            radius: Compose::compose(a.radius, b.radius),
-            weights: Compose::compose(a.weights, b.weights),
-        }
-    }
-}
-
-#[derive(Clone)]
-pub struct Coreset {
-    /// the size of the coreset, i.e. the number of proxy
-    /// points to be selected
-    tau: usize,
-    /// the function to compute the weight of each proxy point
-    coreset_fit: Option<CoresetFit>,
-}
-
-impl Coreset {
-    pub fn new(tau: usize) -> Self {
-        Self {
-            tau,
-            coreset_fit: None,
-        }
-    }
-
-    /// Compute the coreset points and their weights. Return the array with the assignment of input
-    /// data points to the closest coreset point, i.e. the proxy function.
-    pub fn fit_predict<S: Data<Elem = f32>, A>(
-        &mut self,
-        data: &ArrayBase<S, Ix2>,
-        ancillary: Option<AncillaryInfo<A>>,
-    ) -> Array1<usize> {
-        use crate::gmm::*;
-
-        let (coreset_points, assignment, radius) = greedy_minimum_maximum(data, self.tau);
-
-        // TODO: Here we need to put a call to a function to extend the
-        // coreset points selection
-
-        let weights = if let Some(ancillary) = ancillary {
-            ancillary.compute_weights(&assignment)
-        } else {
-            weight_by_count(&assignment)
-        };
-        self.coreset_fit.replace(CoresetFit {
-            coreset_points: data.select(Axis(0), coreset_points.as_slice().unwrap()),
-            radius,
-            weights,
-        });
-
-        assignment
-    }
-
-    pub fn coreset_points(&self) -> Option<ArrayView2<f32>> {
-        self.coreset_fit.as_ref().map(|cf| cf.coreset_points.view())
-    }
-
-    pub fn coreset_radii(&self) -> Option<ArrayView1<f32>> {
-        self.coreset_fit.as_ref().map(|cf| cf.radius.view())
-    }
-
-    pub fn coreset_weights(&self) -> Option<ArrayViewD<usize>> {
-        self.coreset_fit.as_ref().map(|cf| cf.weights.view())
-    }
-}
-
-impl Compose for Coreset {
-    fn compose(a: Self, b: Self) -> Self {
-        assert!(a.coreset_fit.is_some());
-        assert!(b.coreset_fit.is_some());
-        let coreset_fit = Some(Compose::compose(
-            a.coreset_fit.unwrap(),
-            b.coreset_fit.unwrap(),
-        ));
-        Self {
-            tau: a.tau + b.tau,
-            coreset_fit,
-        }
-    }
-}
-
 pub trait NChunks {
     type Output<'slf>
     where
@@ -448,23 +343,6 @@ impl<T> NChunks for &[T] {
     }
 }
 
-impl<'data, A> NChunks for AncillaryInfo<'data, A> {
-    type Output<'slf> = AncillaryInfo<'slf, A> where Self: 'slf;
-
-    fn nchunks(&self, num_chunks: usize) -> impl Iterator<Item = Self::Output<'_>> {
-        let func = self.compute_weights_fn.clone();
-        let data_chunks = self.ancillary.nchunks(num_chunks);
-        data_chunks.map(move |c| AncillaryInfo {
-            ancillary: c,
-            compute_weights_fn: func.clone(),
-        })
-    }
-
-    fn nchunks_size(&self, _num_chunks: usize) -> usize {
-        unimplemented!()
-    }
-}
-
 impl<C: NChunks> NChunks for Option<C> {
     type Output<'slf> = Option<C::Output<'slf>> where Self: 'slf;
 
@@ -505,72 +383,5 @@ impl NChunks for () {
 
     fn nchunks_size(&self, _num_chunks: usize) -> usize {
         0
-    }
-}
-
-pub struct ParallelCoreset {
-    coresets: Vec<Coreset>,
-    composed_fit: Option<CoresetFit>,
-}
-
-impl ParallelCoreset {
-    pub fn new(tau: usize, threads: usize) -> Self {
-        let mut coresets = Vec::with_capacity(threads);
-        for _ in 0..threads {
-            coresets.push(Coreset::new(tau));
-        }
-        Self {
-            coresets,
-            composed_fit: None,
-        }
-    }
-
-    pub fn fit<S: Data<Elem = f32>, A: Send + Sync>(
-        &mut self,
-        data: &ArrayBase<S, Ix2>,
-        ancillary: Option<AncillaryInfo<A>>,
-    ) {
-        let coresets = &mut self.coresets;
-        let n_chunks = coresets.len();
-        let chunks = data.nchunks(n_chunks);
-        let ancillary_chunks = ancillary.nchunks(n_chunks);
-        std::thread::scope(|scope| {
-            let mut handles = Vec::new();
-            for ((coreset, chunk), ancillary_chunk) in
-                coresets.iter_mut().zip(chunks).zip(ancillary_chunks)
-            {
-                let h = scope.spawn(move || {
-                    coreset.fit_predict(&chunk, ancillary_chunk);
-                });
-                handles.push(h);
-            }
-
-            for h in handles {
-                h.join().unwrap();
-            }
-        });
-
-        eprintln!("Putting the solution together");
-        // put together the solution
-        let mut composed = coresets[0].clone();
-        for coreset in &coresets[1..] {
-            composed = Compose::compose(composed, coreset.clone());
-        }
-
-        self.composed_fit.replace(composed.coreset_fit.unwrap());
-    }
-
-    pub fn coreset_points(&self) -> Option<ArrayView2<f32>> {
-        self.composed_fit
-            .as_ref()
-            .map(|cf| cf.coreset_points.view())
-    }
-
-    pub fn coreset_radii(&self) -> Option<ArrayView1<f32>> {
-        self.composed_fit.as_ref().map(|cf| cf.radius.view())
-    }
-
-    pub fn coreset_weights(&self) -> Option<ArrayViewD<usize>> {
-        self.composed_fit.as_ref().map(|cf| cf.weights.view())
     }
 }
